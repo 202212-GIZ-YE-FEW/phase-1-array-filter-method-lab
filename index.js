78df99933c205a76bf810b9f1cff9f3e07ebbb52
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-02-27 11:19:49 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+findMatching = (drivers,_strings)=>{
+return drivers.filter(
+  (possibleMatch)=> possibleMatch.toLowerCase() === _strings.toLowerCase()
+);
+}
+
+function fuzzyMatch(drivers, testString) {
+  return drivers.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0 
+  );
+}
+
+function matchName(drivers, soughtName) {
+  return drivers.filter((record) => record.name === soughtName);
+}
